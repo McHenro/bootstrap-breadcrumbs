4fd8b372401b2a1ecb4b17052cb4f0000e714d9f
Henry Anorue <hanorue@drohealth.com>
2024-08-04 21:44:39 +0100

    Update setup.py to support Django 4.x and Python 3.8+

--- setup.py
+++ setup.py
@@ -5,11 +5,8 @@
     :contact: l.mierzwa@gmail.com
 """
 
-
 from __future__ import unicode_literals
-
 from setuptools import setup, find_packages
-
 
 setup(
     name='django_bootstrap_breadcrumbs',
@@ -17,19 +14,26 @@ setup(
     url='http://prymitive.github.com/bootstrap-breadcrumbs',
     license='MIT',
     description='Django breadcrumbs for Bootstrap 2, 3 or 4',
-    long_description='Django template tags used to generate breadcrumbs html '
-                     'using bootstrap css classes or custom template',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
     author='Łukasz Mierzwa',
     author_email='l.mierzwa@gmail.com',
     packages=find_packages(exclude=['tests']),
     install_requires=[
         'six',
+        'Django>=4.0',  # Add Django as a dependency
     ],
     classifiers=[
         'License :: OSI Approved :: MIT License',
         'Intended Audience :: Developers',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Framework :: Django',
+        'Framework :: Django :: 4.0',
+        'Framework :: Django :: 4.1',
+        'Framework :: Django :: 4.2',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
     platforms=['any'],
